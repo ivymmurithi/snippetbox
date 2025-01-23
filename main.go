50ec56c708c026cbd155bccdfb7b3846794edfee
ivymmurithi <ivymurithi@gmail.com>
2025-01-23 20:14:33 +0300

    url query params

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Handler
@@ -16,10 +18,26 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet ..."))
+
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	// w.Write([]byte("Display a specific snippet ..."))
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "POST"{
+		// below must be called for all errors codes except 200
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
+
 	w.Write([]byte("Create a new snippet ..."))
 }
 
