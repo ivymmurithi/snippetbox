c7b30ffad52dd02294719accd13b9608c547b72d
ivymmurithi <ivymurithi@gmail.com>
2025-01-23 15:53:29 +0300

    (routing): defaultservemux and routing

--- main.go
+++ main.go
@@ -7,7 +7,20 @@ import (
 
 // Handler
 func home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w,r)
+		return
+	}
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet ..."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet ..."))
 }
 
 // Router
@@ -15,6 +28,10 @@ func main() {
 	// entry point of requests
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	//! mux.HandleFunc("/snippet", showSnippet) can work without the initialized serve mux
+	//! but it is not secure because it uses the dafault serve mux which is a global variable that can be accessed by 3rd parties
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Starting server on :4000")
 	// "host:port"
