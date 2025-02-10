05f932180158ce7b20d2b0d13e00871822688a0f
ivymmurithi <ivymurithi@gmail.com>
2025-02-10 17:12:06 +0300

    (feat):initialize database connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 type application struct {
@@ -13,11 +18,24 @@ type application struct {
 }
 
 func main() {
+	err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+	}
+	databasePassword := os.Getenv("DATABASE_PASSWORD")
+
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := fmt.Sprintf("web:%s@/snippetbox?parseTime=true", databasePassword)
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -31,6 +49,17 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err =db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
